5a7bd8fb0c245256e1ffb3e16314d3a015180829
李雪洋 <lixueyang@qianmi.com>
2018-01-15 17:29:51 +0800

    class增加属性问题

--- src/modules/component/left.vue
+++ src/modules/component/left.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="slide-left">
         <ul>
-            <li :class="{active: chooseTab === 'booklist'}">
+            <!--1.多属性判断需要拆开写-->
+            <!--<li class="active" :class="{on: chooseTab === 'booklist'}">-->
+            <!--2.去掉大括号，直接三目运算-->
+            <li :class="chooseTab === 'booklist' ? 'active' : ''">
                 <router-link :to="{path:'/booklist'}"><a href="javascript:void(0)" title="图书列表">图书列表</a></router-link>
             </li>
             <li :class="{active: chooseTab === 'search'}">
